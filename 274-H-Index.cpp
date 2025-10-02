1d5fc4189ec266713bc90d6dfec58705c36e4894
Yaswanth Ravi Teja Dammalapati <102867552+Yash8576@users.noreply.github.com>
2025-10-02 15:22:27 -0400

    Added Link

--- 274-H-Index.cpp
+++ 274-H-Index.cpp
@@ -1,18 +1,20 @@
 int hIndex(vector<int>& citations) {
-        int index =1;
-        int ans = 0;
-        for(int i=0;i<citations.size();i++){
-            int count = 0;
-            int oldindex = index;
-            for(int j=0;j<citations.size();j++){
-                if(citations[j]>=index) count++;
-                if(count>=index){
-                    ans = index;
-                    index++;
-                    break;
-                }
+    int index =1;
+    int ans = 0;
+    for(int i=0;i<citations.size();i++){
+        int count = 0;
+        int oldindex = index;
+        for(int j=0;j<citations.size();j++){
+            if(citations[j]>=index) count++;
+            if(count>=index){
+                ans = index;
+                index++;
+                break;
             }
-            if(oldindex==index) break;
         }
-        return ans;
+        if(oldindex==index) break;
     }
+    return ans;
+}
+
+//Link: https://leetcode.com/problems/h-index/description/
