556bab38611a1412bdc5e7461e557f39cf5cefea
Yaswanth Ravi Teja Dammalapati <102867552+Yash8576@users.noreply.github.com>
2026-01-05 20:45:51 -0500

    Implement function to calculate V-shaped diagonal length

---
+++ Code 3459-Length_of_Longest_V-_Shaped_Diagonal_Segment.cpp
@@ -0,0 +1,49 @@
+int lenOfVDiagonal(vector<vector<int>>& grid){
+    static int dp[501][501][4][2];
+    memset(dp, 0, sizeof(dp));
+    int n = grid.size(), m = grid[0].size();
+    vector<vector<int>> dir = {{1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
+    for(int d = 0; d<4; d++){
+        int istart = (dir[d][0]==1)?n-1:0;
+        int iend = (dir[d][0] == 1)?-1:n;
+        int istep = (dir[d][0] == 1)?-1:1;
+
+        int jstart = (dir[d][1]==1)?m-1:0;
+        int jend = (dir[d][1] == 1)?-1:m;
+        int jstep = (dir[d][1] == 1)?-1:1;
+        for(int i = istart; i!=iend; i+=istep){
+            for(int j = jstart; j!=jend; j+=jstep){
+                for(int t = 0; t<2; t++){
+                    int target = (t==1?2:0);
+                    if(grid[i][j]!=target) continue;
+                    int ni = i + dir[d][0], nj = j + dir[d][1];
+                    if(ni>=0 && nj>=0 && ni<n &&nj<m) dp[i][j][d][t] = 1+dp[ni][nj][d][1-t];
+                    else dp[i][j][d][t] = 1;
+                }
+            }
+        }
+    }
+    int res = 0;
+    for(int i=0; i<n; i++){
+        for(int j = 0; j<m; j++){
+            if(grid[i][j] != 1) continue;
+            res = max(res, 1);
+            for(int d = 0; d<4;d++){
+                int dx = dir[d][0], dy = dir[d][1];
+                int ni = i+dx, nj = j+dy;
+                if(ni<0||nj<0||ni>=n||nj>=m) continue;
+                int maxstraight = dp[ni][nj][d][1];
+                res = max(res, 1+maxstraight);
+                for(int len = 1; len<=maxstraight; len++){
+                    int endi = i+dx*len, endj = j+dy*len;
+                    int nd = (d+1)%4;
+                    int ti = endi + dir[nd][0], tj = endj + dir[nd][1];
+                    if (ti < 0 || tj < 0 || ti >= n || tj >= m) continue;
+                    int next_t = (len%2==1?0:1);
+                    res = max(res, 1+len+dp[ti][tj][nd][next_t]);
+                }
+            }
+        }
+    }
+    return res;
+}
