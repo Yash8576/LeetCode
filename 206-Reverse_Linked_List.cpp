4810b1fc020cdc27ff63ec196f72cb6817e9bf8e
Yaswanth Ravi Teja Dammalapati <102867552+Yash8576@users.noreply.github.com>
2026-01-08 11:22:22 -0500

    Implemented function to reverse a linked list

---
+++ 206-Reverse_Linked_List.cpp
@@ -0,0 +1,11 @@
+ListNode* ReverseList(ListNode* head){
+    ListNode* prev = nullptr;
+    ListNode* curr = head;
+    while(curr!=nullptr){
+        ListNode* temp = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = temp;
+    }
+    return prev;
+}
